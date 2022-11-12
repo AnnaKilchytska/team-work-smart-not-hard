8377a59444407a5ee387848e03900b9949ad6b2e
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2022-11-12 13:43:07 +0200

    additional code

--- src/js/buy-now-modal.js
+++ src/js/buy-now-modal.js
@@ -15,3 +15,18 @@
     refs.body.classList.toggle('body--modal');
   }
 })();
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-buy-now-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-buy-now-close]'),
+//     modal: document.querySelector('[data-modal-buy-now]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// })();
